7054f5841a9d19100cf61825c9a3cd5c63a15c51
Luke Chin <138675471+EkulNich@users.noreply.github.com>
2026-08-21 06:56:36 +0800

    Level-0

---
+++ src/main/java/Lune.java
@@ -0,0 +1,10 @@
+public class Lune {
+    public static void main(String[] args) {
+        String banner = " _                     \n"
+                + "| |   _   _ _ __   ___ \n"
+                + "| |  | | | | '_ \\ / _ \\\n"
+                + "| |__| |_| | | | |  __/\n"
+                + "|_____\\__,_|_| |_|\\___|\n";
+        System.out.println(banner);
+    }
+}
