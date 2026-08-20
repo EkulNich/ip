32f6041d15656c67d0a9926b9088c9d264e56e16
Luke Chin <138675471+EkulNich@users.noreply.github.com>
2026-08-21 07:12:09 +0800

    Level-1

--- src/main/java/Lune.java
+++ src/main/java/Lune.java
@@ -1,4 +1,9 @@
+import java.util.Scanner;
+
 public class Lune {
+    private static final String LINE =
+            "    ____________________________________________________________\n";
+
     public static void main(String[] args) {
         String banner = " _                     \n"
                 + "| |   _   _ _ __   ___ \n"
@@ -6,5 +11,19 @@ public class Lune {
                 + "| |__| |_| | | | |  __/\n"
                 + "|_____\\__,_|_| |_|\\___|\n";
         System.out.println(banner);
+
+        System.out.println(LINE + "     Hello! I'm Lune\n     What can I do for you?\n" + LINE);
+
+        // Scanner is enough here since input is just read line-by-line;
+        // no need for buffered/streamed reading at this stage.
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            String input = scanner.nextLine();
+            if (input.equals("bye")) {
+                System.out.println(LINE + "     Bye. Hope to see you again soon!\n" + LINE);
+                break;
+            }
+            System.out.println(LINE + "     " + input + "\n" + LINE);
+        }
     }
 }
