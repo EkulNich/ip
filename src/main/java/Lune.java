26df9b6eeea8305de3abfcd1257fca39a721b3f3
Luke Chin <138675471+EkulNich@users.noreply.github.com>
2026-08-21 07:26:08 +0800

    Level-3

--- src/main/java/Lune.java
+++ src/main/java/Lune.java
@@ -16,7 +16,7 @@ public class Lune {
 
         // Fixed-size array is enough per the spec (no more than 100 tasks,
         // no need to persist to disk at this stage).
-        String[] tasks = new String[100];
+        Task[] tasks = new Task[100];
         int taskCount = 0;
 
         // Scanner is enough here since input is just read line-by-line;
@@ -28,13 +28,23 @@ public class Lune {
                 System.out.println(LINE + "     Bye. Hope to see you again soon!\n" + LINE);
                 break;
             } else if (input.equals("list")) {
-                StringBuilder listing = new StringBuilder();
+                StringBuilder listing = new StringBuilder("     Here are the tasks in your list:\n");
                 for (int i = 0; i < taskCount; i++) {
-                    listing.append("     ").append(i + 1).append(". ").append(tasks[i]).append("\n");
+                    listing.append("     ").append(i + 1).append(".").append(tasks[i]).append("\n");
                 }
                 System.out.println(LINE + listing + LINE);
+            } else if (input.startsWith("mark ")) {
+                int index = Integer.parseInt(input.substring("mark ".length()).trim()) - 1;
+                tasks[index].markAsDone();
+                System.out.println(LINE + "     Nice! I've marked this task as done:\n"
+                        + "       " + tasks[index] + "\n" + LINE);
+            } else if (input.startsWith("unmark ")) {
+                int index = Integer.parseInt(input.substring("unmark ".length()).trim()) - 1;
+                tasks[index].markAsUndone();
+                System.out.println(LINE + "     OK, I've marked this task as not done yet:\n"
+                        + "       " + tasks[index] + "\n" + LINE);
             } else {
-                tasks[taskCount] = input;
+                tasks[taskCount] = new Task(input);
                 taskCount++;
                 System.out.println(LINE + "     added: " + input + "\n" + LINE);
             }
